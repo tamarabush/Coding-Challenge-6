2e78637092f419a607423426988be7212859337e
tamarabush <tamara.abush14@outlook.com>
2024-09-19 11:09:22 -0400

    Create displayEmployeeShifts function

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -1,4 +1,4 @@
-//TASK 1 - Create an Employees Array of Employee Objects
+//TASK 1 - Create an Employees Array of Employee Objects:
 
 let employees = [
     {name: 'Lana', shifts: [{day: 'Monday', hours: 7},{day: 'Thursday', hours: 9}]},
@@ -7,3 +7,17 @@ let employees = [
     {name: 'Lisa', shifts: [{day: 'Tuesday', hours: 9},]}
 ];
 
+//TASK 2 - Create a Function to Display Employee Shift Details:
+
+function displayEnployeeShifts (employees) {
+    console.log(`Employee: ${employees.name}`)
+    //this will console the employee name 
+
+    employees.shifts.forEach(shifts =>{
+        console.log(`Day: ${shifts.day}, Hours: ${shifts.hours}`);
+    //using the forEach and arrow method to locate the day shifts in the employees array
+    })};
+
+displayEnployeeShifts(employees[0]);
+
+//TASK 3 - Create a Function to Assign a New Shift:

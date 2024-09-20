33269c794c4fff43b7eca651d7b7ec3f19a8b89c
tamarabush <tamara.abush14@outlook.com>
2024-09-20 13:55:45 -0400

    Create assignShift function

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -21,3 +21,30 @@ function displayEnployeeShifts (employees) {
 displayEnployeeShifts(employees[0]);
 
 //TASK 3 - Create a Function to Assign a New Shift:
+
+function assignShifts(employeeName, dayShift, hours) {
+    let employee = employees.find(e => e.name === employeeName);
+    //used find method to locate employee name in the array
+
+    if (!employee) {
+        console.log(`ERROR: Employee ${employeeName} was not found`);
+        return;
+    //"!" negates if employee was found, therefore will console an error
+    }
+    const shiftsAvailable = employee.shifts.some(shift => shift.day === dayShift);
+    //using the some method to test if the shifts passes in the array 
+    if (shiftsAvailable) {
+        console.log(`ERROR: ${employeeName} is already scheduled on ${dayShift}`);
+        return; 
+    //if the shift already exists in the array that it will console an error
+    }
+    employee.shifts.push({ day: dayShift, hours: hours });
+    //using the push method to input new shifts and hours 
+    console.log(`Shift is now assigned to ${employeeName} on ${dayShift} for ${hours} hours.`);
+    //this will console a new shift for an employee if they are available 
+}
+assignShifts('Harry', 'Thursday', 9);
+assignShifts('Lana', 'Monday', 6);
+assignShifts('Kanye', 'Thursday', 9);
+
+
